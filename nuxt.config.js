771daf45b907478615f0657e00a572f8f9c3547c
Sherkhan <sherkhancrs@gmail.com>
2018-08-16 16:56:50 +0600

    toaster

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,97 +1,110 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: pkg.description
-      }
-    ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
-  },
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: pkg.name,
+		meta: [{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: pkg.description
+			}
+		],
+		link: [{
+			rel: 'icon',
+			type: 'image/x-icon',
+			href: '/favicon.ico'
+		}]
+	},
 
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#FFFFFF'
-  },
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: {
+		color: '#FFFFFF'
+	},
 
-  /*
-   ** Global CSS
-   */
-  css: [
-    "~assets/css/main.css"
-  ],
+	/*
+	 ** Global CSS
+	 */
+	css: [
+		"~assets/css/main.css"
+	],
 
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    "~/plugins/backend",
-    { src: "~/plugins/swiper", ssr: false },
-    { src: "~/plugins/pagination", ssr: false }
-  ],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [
+		"~/plugins/backend",
+		{
+			src: "~/plugins/swiper",
+			ssr: false
+		},
+		{
+			src: "~/plugins/pagination",
+			ssr: false
+		}
+	],
 
-  /*
-   ** Nuxt.js modules
-   */
+	/*
+	 ** Nuxt.js modules
+	 */
 
-  router: {
-    scrollBehavior: function (to, from, savedPosition) {
-      return {
-        x: 0,
-        y: 0
-      };
-    }
-  },
+	router: {
+		scrollBehavior: function (to, from, savedPosition) {
+			return {
+				x: 0,
+				y: 0
+			};
+		}
+	},
 
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
-  ],
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
+	modules: [
+		// Doc: https://github.com/nuxt-community/axios-module#usage
+		'@nuxtjs/axios',
+		'@nuxtjs/toast'
+	],
 
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+	toast: {
+    position: 'top-center',
+    duration: 2000
+	},
+
+	/*
+	 ** Axios module configuration
+	 */
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
+	},
+
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {
+			// Run ESLint on save
+			if (ctx.isDev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	}
 }
